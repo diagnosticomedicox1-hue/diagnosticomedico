5ee82ac96791708e9981adb843310f66245016ec
diagnosticomedicox1 <diagnosticomedicox1@gmail.com>
2026-01-27 15:28:37 -0600

    fix: revert standalone and install netlify plugin

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
     reactStrictMode: false,
     serverExternalPackages: ['@libsql/client'],
     webpack: (config, { isServer }) => {
